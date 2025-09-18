40c24159575a110a590e2c6085a7cf801bde500b
Fayyad Gosein <alfayyadgos@gmail.com>
2025-09-18 02:27:35 +0000

    the tutorial is not formatted properly fix it it takes up more than the

--- src/components/tutorial-view.tsx
+++ src/components/tutorial-view.tsx
@@ -11,6 +11,7 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/componen
 import { Button } from '@/components/ui/button';
 import { Barcode, ScanText, CheckCircle2, PackageCheck } from 'lucide-react';
 import { HalalEatsLogo } from './halal-eats-logo';
+import { Dialog, DialogContent } from '@/components/ui/dialog';
 
 interface TutorialViewProps {
   onFinish: () => void;
@@ -41,8 +42,8 @@ export function TutorialView({ onFinish }: TutorialViewProps) {
   ];
 
   return (
-    <Dialog open={true}>
-        <DialogContent className="sm:max-w-[425px] p-0 border-0" onPointerDownOutside={(e) => e.preventDefault()}>
+    <Dialog open={true} onOpenChange={(isOpen) => { if (!isOpen) onFinish(); }}>
+        <DialogContent className="sm:max-w-[425px] p-0 border-0" onPointerDownOutside={(e) => e.preventDefault()} onEscapeKeyDown={(e) => e.preventDefault()}>
             <Carousel className="w-full">
             <CarouselContent>
                 {tutorialSteps.map((step, index) => (
@@ -64,65 +65,17 @@ export function TutorialView({ onFinish }: TutorialViewProps) {
                             ) : (
                                 <CarouselNext className="w-full static translate-x-0" />
                             )}
+                             <div className="h-8">
+                                {index < tutorialSteps.length - 1 && <CarouselPrevious className="static translate-x-0" />}
+                            </div>
                         </CardFooter>
                     </Card>
                     </div>
                 </CarouselItem>
                 ))}
             </CarouselContent>
-            <div className="absolute bottom-4 left-1/2 -translate-x-1/2">
-                <CarouselPrevious className="static translate-x-0" />
-            </div>
             </Carousel>
         </DialogContent>
     </Dialog>
   );
 }
-
-
-// These are needed because the TutorialView is not wrapped in a form, but uses Dialog
-import * as React from "react"
-import * as DialogPrimitive from "@radix-ui/react-dialog"
-import { X } from "lucide-react"
-
-import { cn } from "@/lib/utils"
-
-const Dialog = DialogPrimitive.Root
-
-const DialogPortal = DialogPrimitive.Portal
-
-const DialogOverlay = React.forwardRef<
-  React.ElementRef<typeof DialogPrimitive.Overlay>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
->(({ className, ...props }, ref) => (
-  <DialogPrimitive.Overlay
-    ref={ref}
-    className={cn(
-      "fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-      className
-    )}
-    {...props}
-  />
-))
-DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
-
-const DialogContent = React.forwardRef<
-  React.ElementRef<typeof DialogPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({ className, children, ...props }, ref) => (
-  <DialogPortal>
-    <DialogOverlay />
-    <DialogPrimitive.Content
-      ref={ref}
-      className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
-        className
-      )}
-      {...props}
-    >
-      {children}
-    </DialogPrimitive.Content>
-  </DialogPortal>
-))
-DialogContent.displayName = DialogPrimitive.Content.displayName
-
