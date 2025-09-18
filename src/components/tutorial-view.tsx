e7c930fd4235ecf2857d15be1e34322d2387471f
Fayyad Gosein <alfayyadgos@gmail.com>
2025-09-18 02:34:44 +0000

    fix the tutorial as on phones the tutorial doesnt even show properly on

--- src/components/tutorial-view.tsx
+++ src/components/tutorial-view.tsx
@@ -43,7 +43,7 @@ export function TutorialView({ onFinish }: TutorialViewProps) {
 
   return (
     <Dialog open={true} onOpenChange={(isOpen) => { if (!isOpen) onFinish(); }}>
-        <DialogContent className="sm:max-w-[425px] p-0 border-0" onPointerDownOutside={(e) => e.preventDefault()} onEscapeKeyDown={(e) => e.preventDefault()}>
+        <DialogContent className="max-w-[90vw] sm:max-w-md p-0 border-0 rounded-lg" onPointerDownOutside={(e) => e.preventDefault()} onEscapeKeyDown={(e) => e.preventDefault()}>
             <Carousel className="w-full">
             <CarouselContent>
                 {tutorialSteps.map((step, index) => (
